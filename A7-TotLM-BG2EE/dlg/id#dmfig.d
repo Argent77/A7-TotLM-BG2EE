eccdc207424054d99a99de00316381124d147556
Argent77 <4519923+Argent77@users.noreply.github.com>
2021-08-01 19:27:39 +0200

    Improvements and fixes for IWD spells

--- A7-TotLM-BG2EE/dlg/id#dmfig.d
+++ A7-TotLM-BG2EE/dlg/id#dmfig.d
@@ -1,11 +1,11 @@
 BEGIN ~ID#DMFIG~
 
 IF ~Global("Gone","LOCALS",0) Name("MumFig2",Myself)~ Cleric
-  SAY @91111 /* Ahh! Thank you for releasing me from that curse. But I already feel the weight of the centuries, pulling me away from this place. Now I can finally rest in peace. */
+  SAY @91110 /* Ahh! Thank you for releasing me from that curse. But I already feel the weight of the centuries, pulling me away from this place. Now I can finally rest in peace. */
   IF ~~ DO ~SetGlobal("Gone","LOCALS",1) AddXPWorth(Myself) ApplySpellRES("IDSP056",Myself)~ EXIT
 END
 
 IF ~Global("Gone","LOCALS",0) !Name("MumFig2",Myself)~ Acolyte
-  SAY @91112 /* Wha—?! Where am I? What has happened?  Ugh, too weak... can't move. */
+  SAY @91111 /* Wha—?! Where am I? What has happened?  Ugh, too weak... can't move. */
   IF ~~ DO ~SetGlobal("Gone","LOCALS",1) AddXPWorth(Myself) ApplySpellRES("IDSP056",Myself)~ EXIT
 END
